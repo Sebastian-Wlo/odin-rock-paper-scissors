37e08969632985a34101ba40fc32d02b431e5d9b
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-03-21 23:17:51 +0100

    Add random outcome generating function (getComputerChoice)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("Hello from the external js file");
+function getComputerChoice() {
+    const max = 3;
+    let shape = "";
+    let outcome = Math.floor(Math.random() * max);
+    switch (outcome) {
+        case 1:
+            shape = "rock";
+            break;
+        case 2:
+            shape = "paper";
+            break
+        default:
+            shape = "scissors";
+            break;
+        };
+    return shape
+}
