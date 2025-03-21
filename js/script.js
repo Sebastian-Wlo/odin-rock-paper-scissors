fe7239ef669b2f2802b260abbb01c8320bfd308f
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-03-21 23:49:02 +0100

    Add basic game functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-console.log("Hello from the external js file");
+let humanScore = 0;
+let computerScore = 0;
+
+// console.log("Hello from the external js file");
 function getComputerChoice() {
     const max = 3;
     let shape = "";
@@ -16,3 +19,57 @@ function getComputerChoice() {
         };
     return shape
 }
+
+function getHumanChoice() {
+    let userChoice = prompt("Rock, Paper or Scissors?").toLowerCase().trim();
+    return userChoice;
+}
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+
+    console.log(`Player choice: ${humanChoice}, Computer choice: ${computerChoice}`);
+    if (humanChoice === computerChoice) {
+        console.log("Draw!");
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("Computer wins the round!");
+            computerScore += 1;
+        } else if (computerChoice === "scissors") {
+            console.log("Player wins the round!");
+            humanScore += 1;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("Computer wins the round!");
+            computerScore += 1;
+        } else if (computerChoice === "rock") {
+            console.log("Player wins the round!");
+            humanScore += 1;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("Computer wins the round!");
+            computerScore += 1;
+        } else if (computerChoice === "paper") {
+            console.log("Player wins the round!");
+            humanScore += 1;
+        }
+    }
+
+    console.log(`Player Score: ${humanScore}, Computer Score: ${computerScore}`);
+}
+function playGame() {
+    do {
+        playRound();
+    } while (humanScore < 3 && computerScore < 3);
+    if (humanScore === 3) {
+        console.log("Player Wins the Game!");
+    } else {
+        console.log("Computer Wins the Game!");
+    }
+}
+
+playGame();
